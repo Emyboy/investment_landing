7cb6299e16b09893a841e58cb46260776aa06bdd
Emy Ifeora <chukwuemekaifeora@gmail.com>
2022-08-13 13:01:41 +0100

    ...

--- components/About/TopInvestors.vue
+++ components/About/TopInvestors.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="investor-section padding-bottom overflow-hidden pt-5">
+    <section class="investor-section padding-bottom overflow-hidden pt-5">
         <div class="container">
             <div class="row justify-content-center">
                 <div class="col-lg-7">
@@ -7,16 +7,24 @@
                         <span class="subtitle">featured investors</span>
                         <h2 class="title">our top investor</h2>
                         <p>
-                            Pipsum dolor sit amet consectetur adipisicing elit. Aliquam modi explicabo nam ex unde et dolorum non dolor! Dolorum nobis
+                            Pipsum dolor sit amet consectetur adipisicing elit.
+                            Aliquam modi explicabo nam ex unde et dolorum non
+                            dolor! Dolorum nobis
                         </p>
                     </div>
                 </div>
             </div>
             <div class="row justify-content-center gy-5">
-                <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6  slideInUp" data--delay=".3s">
+                <div
+                    class="col-xl-3 col-lg-4 col-md-6 col-sm-6 slideInUp"
+                    data--delay=".3s"
+                >
                     <div class="investor-item">
                         <div class="investor-thumb">
-                            <img src="~assets/images/investor/item1.png" alt="investor">
+                            <img
+                                src="~assets/images/investor/item1.png"
+                                alt="investor"
+                            />
                         </div>
                         <div class="investor-content">
                             <h4 class="name">Robart Betson</h4>
@@ -25,10 +33,16 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6  slideInUp" data--delay=".4s">
+                <div
+                    class="col-xl-3 col-lg-4 col-md-6 col-sm-6 slideInUp"
+                    data--delay=".4s"
+                >
                     <div class="investor-item">
                         <div class="investor-thumb">
-                            <img src="~assets/images/investor/item2.png" alt="investor">
+                            <img
+                                src="~assets/images/investor/item2.png"
+                                alt="investor"
+                            />
                         </div>
                         <div class="investor-content">
                             <h4 class="name">Robart Betson</h4>
@@ -37,10 +51,16 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6  slideInUp" data--delay=".5s">
+                <div
+                    class="col-xl-3 col-lg-4 col-md-6 col-sm-6 slideInUp"
+                    data--delay=".5s"
+                >
                     <div class="investor-item">
                         <div class="investor-thumb">
-                            <img src="~assets/images/investor/item3.png" alt="investor">
+                            <img
+                                src="~assets/images/investor/item3.png"
+                                alt="investor"
+                            />
                         </div>
                         <div class="investor-content">
                             <h4 class="name">Robart Betson</h4>
@@ -49,10 +69,16 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6  slideInUp" data--delay=".6s">
+                <div
+                    class="col-xl-3 col-lg-4 col-md-6 col-sm-6 slideInUp"
+                    data--delay=".6s"
+                >
                     <div class="investor-item">
                         <div class="investor-thumb">
-                            <img src="~assets/images/investor/item4.png" alt="investor">
+                            <img
+                                src="~assets/images/investor/item4.png"
+                                alt="investor"
+                            />
                         </div>
                         <div class="investor-content">
                             <h4 class="name">Robart Betson</h4>
@@ -67,11 +93,7 @@
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
-<style>
-
-</style>
+<style></style>
